ee3d7feef56f22cb0b540c99001601d323f74aec
eldavenport <ellendavenport7@gmail.com>
2026-06-04 10:14:14 -0700

    adding validation across mixing parameterizations

--- convert_modelProfTP24.m
+++ convert_modelProfTP24.m
@@ -3,8 +3,8 @@ clear variables
 addpath '/home/averdy/valid_matlab_needs/'
 
 prof_data = '/data/SO3/edavenport/tpose24/profiles/'; % where the original profiles are (these are in several places, this begin one of them)
-model_prof_data = '/data/SO3/edavenport/tpose24/oct2012_TP6Vel_3month/PROF/'; % where the model equivalent profiles are
-output_netcdf_data = '/data/SO3/edavenport/tpose24/oct2012_TP6Vel_3month/PROF_eq/'; % where to put the converted profiles
+model_prof_data = '/data/SO3/edavenport/tpose24/oct2012_3month_transp_cons/PROF/'; % where the model equivalent profiles are
+output_netcdf_data = '/data/SO3/edavenport/tpose24/oct2012_3month_transp_cons/PROF_eq/'; % where to put the converted profiles
 
 list_model = {['TAO_WO_2012_ADCP_v2'],['TAO_WO_2012_CUR_v2'],['TAO_WO_2012_CTD_daily_ED'],['ADCP_prof_140'],['ADCP_prof_50'],['fastCTD_prof'],['FCTD_1min']};
 MITprof_gcm2nc(prof_data,model_prof_data,output_netcdf_data,list_model);
